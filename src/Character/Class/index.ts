a2d7be4c03d880df8a532418bbf6153d904ace0b
Edurne <herenvardo@gmail.com>
2024-03-03 20:57:35 +0100

    chore: replace ClassTier enum with ref to ts-enums

--- src/Character/Class/index.ts
+++ src/Character/Class/index.ts
@@ -1,9 +1,8 @@
-import { Aura } from '@wowfinder/ts-enums';
+import { Aura, ClassTier } from '@wowfinder/ts-enums';
 import { Money } from '../../Item';
 import { Skill } from '../Skill';
 import { AurasList } from './Aura';
 import { ClassBonuses } from './ClassBonuses';
-import { ClassTier } from './ClassTier';
 import { ClassFeature, FeaturesList } from './Features';
 import { ClassBuilder, applyClassDefaults } from './builder';
 import { combineClassBonuses } from './combineBonuses';
