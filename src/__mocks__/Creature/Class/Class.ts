55ea250f3a07bd455147e974b0bf74b417909211
Edurne Pascual <herenvardo@gmail.com>
2025-04-05 18:38:24 +0200

    feat: add full class mocks

---
+++ src/__mocks__/Creature/Class/Class.ts
@@ -0,0 +1,16 @@
+import { Class } from '../../../Creature';
+import * as rawMocks from './RawClassAsset';
+
+const mockArcaneClass = new Class(rawMocks.mockArcaneClassRawAsset);
+const mockDivineClass = new Class(rawMocks.mockDivineClassRawAsset);
+const mockMeleeClass = new Class(rawMocks.mockMeleeClassRawAsset);
+const mockStealthClass = new Class(rawMocks.mockStealthClassRawAsset);
+const mockSuperHybridClass = new Class(rawMocks.mockSuperHybridClassRawAsset);
+
+export {
+    mockArcaneClass,
+    mockDivineClass,
+    mockMeleeClass,
+    mockStealthClass,
+    mockSuperHybridClass,
+};
