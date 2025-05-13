faaf1da4c639c3a5916ffbcd3a325b84a34347a9
Edurne Pascual <herenvardo@gmail.com>
2025-05-13 13:16:49 +0200

    feat: implement addVitals function to combine vitals profiles and bonuses

--- src/Profile/VitalsProfile.ts
+++ src/Profile/VitalsProfile.ts
@@ -1,5 +1,6 @@
 import { type Counter } from '@wowfinder/ts-utils';
 import { days, hours, minutes, Time } from '../Scalar';
+import { VitalsBonus } from '../Bonus';
 
 type SleepProfile = {
     minimumSleepTime: Time;
@@ -31,4 +32,61 @@ type VitalsProfile = {
     breath?: BreathProfile;
 };
 
-export { type VitalsProfile, defaultBreathProfile, defaultSleepProfile };
+function addVitals(
+    base: VitalsProfile,
+    ...bonuses: VitalsBonus[]
+): VitalsProfile {
+    const totalBonuses = VitalsBonus.sum(...bonuses);
+    const hp = {
+        ...base.hp,
+        current: base.hp.current + totalBonuses.maxHpBonus,
+        max: base.hp.max + totalBonuses.maxHpBonus,
+    };
+    const sanity = {
+        ...base.sanity,
+        current: base.sanity.current + totalBonuses.maxSanityBonus,
+        max: base.sanity.max + totalBonuses.maxSanityBonus,
+    };
+    const baseSleep = base.sleep ?? defaultSleepProfile;
+    const baseBreath = base.breath ?? defaultBreathProfile;
+    return {
+        hp,
+        sanity,
+        sleep: {
+            minimumSleepTime: Time.add(
+                baseSleep.minimumSleepTime.unit,
+                baseSleep.minimumSleepTime,
+                totalBonuses.sleepTimeReduction.negative,
+            ),
+            optimalSleepTime: Time.add(
+                baseSleep.optimalSleepTime.unit,
+                baseSleep.optimalSleepTime,
+                totalBonuses.sleepTimeReduction.negative,
+            ),
+            sleepCycle: Time.add(
+                baseSleep.sleepCycle.unit,
+                baseSleep.sleepCycle,
+                totalBonuses.sleepCycleBonus,
+            ),
+        },
+        breath: {
+            breathHoldingTime: Time.add(
+                baseBreath.breathHoldingTime.unit,
+                baseBreath.breathHoldingTime,
+                totalBonuses.breathHoldingTimeBonus,
+            ),
+            breathRecoveryTime: Time.add(
+                baseBreath.breathRecoveryTime.unit,
+                baseBreath.breathRecoveryTime,
+                totalBonuses.breathRecoveryTimeReduction.negative,
+            ),
+        },
+    };
+}
+
+export {
+    type VitalsProfile,
+    defaultBreathProfile,
+    defaultSleepProfile,
+    addVitals,
+};
