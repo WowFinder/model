96175c01ddc664fc9431cab2a60bc39834ce009a
Edurne Pascual <herenvardo@gmail.com>
2025-08-06 01:50:45 +0200

    test: update error assertion

--- src/Transformation/Shapeshift/__tests__/base.test.ts
+++ src/Transformation/Shapeshift/__tests__/base.test.ts
@@ -24,7 +24,7 @@ describe('Shapeshift', () => {
     describe('apply', () => {
         it('should throw a NotImplemented error', () => {
             const shapeshift = new ShapeshiftTest({ rank: 1 });
-            expect(() => shapeshift.apply({})).toThrowError(
+            expect(() => shapeshift.apply({})).toThrow(
                 'Not implemented yet',
             );
         });
