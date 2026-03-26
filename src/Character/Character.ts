8b7a5c8a1739a247d2fbc0093ee6198d56d65574
Edurne Pascual <herenvardo@gmail.com>
2026-03-26 16:50:17 +0100

    feat(Character): add many initialziations within `Character` constructor and document missing ones.

--- src/Character/Character.ts
+++ src/Character/Character.ts
@@ -1,25 +1,52 @@
-import { type RawCharacterAsset } from '@wowfinder/asset-schemas';
+import {
+    type RawSkills,
+    type RawCharacterAsset,
+} from '@wowfinder/asset-schemas';
 import { MultiBonus } from '../Bonus';
 import { type AsyncAssetResolver } from '../Assets';
 import {
+    buildProgressionProfile,
     defaultCreatureProfile,
+    SpeedsProfile,
     type CreatureProfile,
     type CreatureProfileOverride,
 } from '../Profile';
 import { Inventory } from '../Item/Inventory';
 import { CharacterCore, type CharacterCoreBuilder } from './CharacterCore';
+import { statMod } from '../Creature';
+import { fillSkills } from '../Creature/Skill/Skills';
+
+type CharacterBuilder = CharacterCoreBuilder & {
+    xp?: number;
+    skillRanks?: RawSkills;
+};
 
 class Character extends CharacterCore {
     readonly #baseProfile: CreatureProfile;
     #overrides: CreatureProfileOverride[] = [];
     #inventory: Inventory = Inventory.empty;
 
-    constructor(args: CharacterCoreBuilder) {
+    constructor({ ...args }: CharacterBuilder) {
         super(args);
+        const stats = args.baseStats;
         this.#baseProfile = {
             ...defaultCreatureProfile,
             personal: args.personal,
-            stats: args.baseStats,
+            // shape: TODO #295
+            size: args.race.size,
+            stats,
+            speeds: new SpeedsProfile({
+                ...args.race.speeds.export(),
+                dexBonus: statMod(stats.dexterity),
+            }),
+            // vitals: TODO #305
+            progression: buildProgressionProfile({
+                xp: args.xp ?? 0, // TODO #306
+                classes: args.classes ?? [],
+            }),
+            skills: fillSkills(args.skillRanks ?? {}), // skills: TODO #307
+            // resistances:  TODO #308
+            traits: defaultCreatureProfile.traits, // traits: TODO #309
         };
     }
 
@@ -44,7 +71,7 @@ class Character extends CharacterCore {
     }
 
     get totalBonuses(): MultiBonus {
-        // TODO #301: Compute from class features, feats, racial bonuses, items, etc.
+        // TODO #301: Compute from class features, feats, items, etc.
         return MultiBonus.zero;
     }
 
